bcc1f3af6579fd32bc19514e32b5bd225badaba8
Vaesea <159406785+Vaesea@users.noreply.github.com>
2026-07-05 23:38:07 +0100

    v1.0.0

--- source/characters/player/Player.hx
+++ source/characters/player/Player.hx
@@ -2,6 +2,7 @@ package characters.player;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.util.FlxColor;
 
 class Player extends FlxSprite
 {
@@ -16,11 +17,14 @@ class Player extends FlxSprite
     var runSpeed:Int = 192;
     var decelerateOnJumpRelease:Float = 0.5;
 
-    // Ducking, unused at the moment
+    // Ducking
     var ducking:Bool = false;
     
     // Direction
     public var direction:Int = 1;
+
+    // Extra Hitbox
+    public var ceilingDetector:FlxSprite; // Really bad way to implement this, I think!
 
     public function new()
     {
@@ -41,10 +45,21 @@ class Player extends FlxSprite
         // Hitbox
         setSize(9, 26);
         offset.set(4, 6);
+        
+        // Ceiling Detector
+        ceilingDetector = new FlxSprite(this.x, this.y - 6);
+        ceilingDetector.makeGraphic(Std.int(this.width), 5, FlxColor.TRANSPARENT);
+
+        // Add Ceiling Detector to PlayState to make it appear in the level
+        Global.PS.add(ceilingDetector);
     }
 
     override public function update(elapsed:Float)
     {
+        // Sync Ceiling Detector with player. This is a bad way to do it, as there's a delay, but who cares?
+        ceilingDetector.x = this.x;
+        ceilingDetector.y = this.y - 6;
+
         // Stop the player from falling off the map through the left
         if (x < 0)
         {
@@ -65,6 +80,8 @@ class Player extends FlxSprite
         // Functions
         move();
         animate();
+
+        FlxG.overlap(Global.PS.map, ceilingDetector);
 
         super.update(elapsed);
     }
@@ -116,17 +133,22 @@ class Player extends FlxSprite
         maxVelocity.x = speed; // Limit the player's speed
 
         // If player presses left, move left. If player presses right, move right.
-        if (FlxG.keys.anyPressed([LEFT, A]))
+        var duck_on_floor = (ducking && isTouching(FLOOR));
+        
+        if (!duck_on_floor)
         {
-            flipX = true; // Flip player
-            direction = -1; // Set direction
-            acceleration.x -= playerAcceleration; // Move
-        }
-        else if (FlxG.keys.anyPressed([RIGHT, D]))
-        {
-            flipX = false; // Unflip player
-            direction = 1; // Set direction
-            acceleration.x += playerAcceleration; // Move
+            if (FlxG.keys.anyPressed([LEFT, A]))
+            {
+                flipX = true; // Flip player
+                direction = -1; // Set direction
+                acceleration.x -= playerAcceleration; // Move
+            }
+            else if (FlxG.keys.anyPressed([RIGHT, D]))
+            {
+                flipX = false; // Unflip player
+                direction = 1; // Set direction
+                acceleration.x += playerAcceleration; // Move
+            }
         }
 
         // If player is pressing jump keys and is on ground, jump.
@@ -149,10 +171,34 @@ class Player extends FlxSprite
         {
             velocity.y -= velocity.y * decelerateOnJumpRelease;
         }
+
+        // Ducking and standing (I wish there was a better way to do this)
+        if (!ducking && isTouching(FLOOR) && FlxG.keys.anyPressed([DOWN, S]))
+        {
+            ducking = true;
+
+            var oldHeight = height;
+            setSize(12, 13);
+            offset.set(4, 19); // Things seem to be reversed here.
+            y += oldHeight - height;
+        }
+        else if (ducking && isTouching(FLOOR) && !FlxG.keys.anyPressed([DOWN, S]))
+        {
+            if (!ceilingDetector.overlaps(Global.PS.map))
+            {
+                ducking = false;
+
+                var oldHeight = height;
+                setSize(9, 26);
+                y -= height - oldHeight;
+                offset.set(4, 6);
+            }
+        }
     }
 
     public function die()
     {
+        Global.coins = 0;
         FlxG.resetState();
     }
 }
